7de77f6f6bcdab7b54f9e7e1df8de71925c826ef
Bunny Blake <sonicforest@gmail.com>
2021-12-10 11:53:26 -0500

    acceleration and friction

--- Player.gd
+++ Player.gd
@@ -1,19 +1,29 @@
 extends KinematicBody2D
 
+const ACCELERATION = 900
+const MAX_SPEED = 300
+const FRICTION = 500
+
 var velocity = Vector2.ZERO
-var speed = 5
+
 
 func _physics_process(delta):
+
+	# input vector
 	var _i = Vector2.ZERO
 	
 	_i.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	_i.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	# normalize vector fixes fast/distorted diagonals
+	_i = _i.normalized()
 
 	if _i != Vector2.ZERO:
-		velocity = _i
+		velocity += _i * ACCELERATION * delta
+		velocity = velocity.clamped(MAX_SPEED)
 	else:
-		velocity = Vector2.ZERO
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
-	move_and_collide(velocity)
+	move_and_collide(velocity * delta)
 
 		
