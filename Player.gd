4b57bf3bdb4459e1b04f934d2a07ad03b055f105
Bunny Blake <sonicforest@gmail.com>
2021-12-11 16:56:14 -0500

    Animation tree, idle poses in all directions

--- Player.gd
+++ Player.gd
@@ -2,9 +2,14 @@ extends KinematicBody2D
 
 const ACCELERATION = 240
 const MAX_SPEED = 90
-const FRICTION = 180
+const FRICTION = 200
 
 var velocity = Vector2.ZERO
+
+
+# onready var means there's less _ready() function needed
+onready var animPlayer = $AnimationPlayer
+onready var animTree = $AnimationTree
 
 # delta is normally 1/60 (.0166667) at 60FPS
 # removing delta slows gameplay with frame drop (might be good?) but
@@ -21,7 +26,14 @@ func _physics_process(delta):
 	# normalize vector fixes fast/distorted diagonals
 	_i = _i.normalized()
 
-	if _i != Vector2.ZERO:
+	if _i != Vector2.ZERO:	
+		
+		# set the blend position on each animation in the tree to match
+		# the input vector
+
+		animTree.set("parameters/Idle/blend_position", _i)
+		animTree.set("parameters/Run/blend_position", _i)
+
 		velocity = velocity.move_toward(_i * MAX_SPEED, ACCELERATION * delta)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
